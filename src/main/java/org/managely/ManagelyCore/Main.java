0f24a93e0fd98c2b6dcabf8a54a6d7d7b416d29c
Your Name <you@example.com>
2021-07-08 21:41:07 -0700

    init

---
+++ src/main/java/org/managely/ManagelyCore/Main.java
@@ -0,0 +1,11 @@
+package org.managely.ManagelyCore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable(){
+        saveDefaultConfig();
+    }
+}
+
