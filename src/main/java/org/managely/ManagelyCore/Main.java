3fd3f441854df82c9a0804fbf66d20cb1cb5cfca
skrekhere <liam@tannock.net>
2021-07-10 12:29:54 -0700

    first event

--- src/main/java/org/managely/ManagelyCore/Main.java
+++ src/main/java/org/managely/ManagelyCore/Main.java
@@ -1,11 +1,19 @@
 package org.managely.ManagelyCore;
 
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.*;
+import net.md_5.bungee.api.chat.hover.content.Text;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class Main extends JavaPlugin {
     @Override
     public void onEnable(){
         saveDefaultConfig();
+
     }
 }
 
